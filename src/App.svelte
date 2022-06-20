aba0ae9979ab798cc17550abe77ed88bfb8159c1
taylor d <taylor.delph@eyecuelab.com>
2022-06-20 13:48:57 -0700

    reactive variable declarations

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,16 @@
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
   let count = 0;
+  $: doubled = count * 2;
 
 	function incrementCount() {
-  
+    count += 1;
   }
 </script>
 
 <button on:click={incrementCount}>
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+
+<p> reactive declaration using $: syntax</p>
+<p>the count: {count} has been doubled: {doubled}</p>
