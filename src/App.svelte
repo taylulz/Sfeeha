f6dfbcfba0a14f5372bc315f3664c2dbabe35def
taylor d <taylor.delph@eyecuelab.com>
2022-06-20 13:44:45 -0700

    bootstrap template

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+  let count = 0;
+
+	function incrementCount() {
+  
+  }
+</script>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
