a227958c015d2bf8a71b3df91c22bbad7ca37a21
taylor d <taylor.delph@eyecuelab.com>
2022-06-20 19:25:25 -0700

    conditional html elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   // import Button from "./tutorial/button.svelte";
   // import Numbies from "./tutorial/numbies.svelte";
-  import Propies from "./tutorial/propies.svelte";
+  // import Propies from "./tutorial/propies.svelte";
+  import Conditionals from "./tutorial/Conditionals.svelte";
 </script>
 
 <!-- <Button/> -->
 <!-- <Numbies/> -->
-<Propies/>
+<!-- <Propies/> -->
+<Conditionals/>
 
