b35fc7440a58948da5c7c00af02a2f75d5fc9540
taylor d <taylor.delph@eyecuelab.com>
2022-06-20 19:42:53 -0700

    Each blocks and lists

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
   // import Numbies from "./tutorial/numbies.svelte";
   // import Propies from "./tutorial/propies.svelte";
   // import Conditionals from "./tutorial/Conditionals.svelte";
-  import IfElse from "./tutorial/IfElse.svelte";
+  // import IfElse from "./tutorial/IfElse.svelte";
+  import EachBlock from "./tutorial/EachBlock.svelte";
 </script>
 
 <!-- <Button/> -->
 <!-- <Numbies/> -->
 <!-- <Propies/> -->
 <!-- <Conditionals/> -->
-<IfElse/>
+<!-- <IfElse/> -->
+<EachBlock/>
 
